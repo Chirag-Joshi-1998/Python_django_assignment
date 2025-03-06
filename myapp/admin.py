b9821fca3d5e2cae5a590fa88b7a99ef96bf586b
Chirag-Joshi-1998 <chikujoshi1998@gmail.com>
2025-03-06 12:19:20 +0530

    8. Django Admin Panel

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Doctor
 # Register your models here.
-@admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_dispaly=('id','name','specialty','experience','contact')
+    list_dispaly=('name','specialty','experience','contact')
+    # list_filter = ('specialty', 'availability')
+    # search_fields = ('name', 'specialty', 'clinic_address')
+
+admin.site.register(Doctor, DoctorAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,11 @@ class Doctor(models.Model):
     specialty = models.CharField(max_length=100)
     experience = models.IntegerField()
     contact = models.CharField(max_length=15)
+    availability = models.BooleanField(default=True)
+    clinic_address = models.TextField()
 
     def __str__(self):
         return self.name
+    
+
+    
