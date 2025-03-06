ecdaad48aaccd11368b9b6c233ac81d2e63c9727
Chirag-Joshi-1998 <chikujoshi1998@gmail.com>
2025-03-06 14:24:30 +0530

    12. ORM and QuerySets CRUD

--- html_in_python/urls.py
+++ html_in_python/urls.py
@@ -24,6 +24,9 @@ urlpatterns = [
     path('doctorlist/', views.doctor_list, name='doctor_list'),
     path('profile/', views.profile, name='profile'),
     path('contactus/', views.contactus, name='contactus'),
+    path('add/', views.add_doctor, name='add_doctor'),
+    path('edit/<int:pk>/',views.edit_doctor, name='edit_doctor'),
+    path('delete/<int:pk>/', views.delete_doctor, name='delete_doctor'),
 
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Doctor
+from .forms import DoctorForm
+
 
 # Create your views here.
 
@@ -9,10 +11,45 @@ def home(request):
 def register(request):
     return render(request, 'myapp/register.html')
 
-
+# READ: Display all doctors
 def doctor_list(request):
     doctors = Doctor.objects.all()  # Fetch all doctors from the database
     return render(request, 'myapp/doctor_list.html', {'doctors': doctors})
+
+
+# CREATE: Add a new doctor
+def add_doctor(request):
+    if request.method == "POST":
+        form = DoctorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('doctor_list')
+    else:
+        form = DoctorForm()
+    return render(request, 'myapp/add_doctor.html', {'form': form})
+
+
+# UPDATE: Edit doctor details
+def edit_doctor(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    if request.method == "POST":
+        form = DoctorForm(request.POST, instance=doctor)
+        if form.is_valid():
+            form.save()
+            return redirect('doctor_list')
+    else:
+        form = DoctorForm(instance=doctor)
+    return render(request, 'myapp/edit_doctor.html', {'form': form})
+
+
+# DELETE: Remove a doctor
+def delete_doctor(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    if request.method == "POST":
+        doctor.delete()
+        return redirect('doctor_list')
+    return render(request, 'myapp/delete_doctor.html', {'doctor': doctor})
+
 
 def profile(request):
     return render(request,'myapp/profile.html')
